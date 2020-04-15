aa201164c15f836f28d72f9b8d0f34604e1f960f
Ian Seng <ianseng@outlook.com>
2020-04-14 21:16:35 -0400

    http

---
+++ http-01-start/src/app/app.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
+import { Post } from './post.model';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    this.http
+      .post<{ name: string }>(
+        'https://http-learn-5279a.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
+
+  onFetchPosts() {
+    this.fetchPosts()
+    // Send Http request
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    this.isFetching = true;
+    this.http
+      .get<{ [key: string]: Post }>('https://http-learn-5279a.firebaseio.com/posts.json')
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key })
+            }
+          }
+          return postsArray;
+        }))
+      .subscribe(post => {
+        console.log(post)
+        this.loadedPosts = post;
+        this.isFetching = false;
+      })
+  }
+}
