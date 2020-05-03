ce35bf838f0c19a0947cb8acf6f6abd1322f076c
Ian Seng <ianseng@outlook.com>
2020-05-02 22:02:13 -0400

    complete reactive form course

--- handling-reactive-form/src/app/app.component.ts
+++ handling-reactive-form/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,64 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenNames = ['test', 'abc'];
+
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'gender': new FormControl('male')
+      'userData' : new FormGroup({
+        'username': new FormControl(null, [Validators.required, this.forbiddenNameCheck.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmailCheck),
+      }),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
+    })
+    // Reacting to status or value change (could be a useful to debut and so on)
+    this.signupForm.get('userData').get('email').valueChanges.subscribe( value => {
+      console.log(value)
+    })
+
+    // Path of set value of the form 
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'hello'
+      }
     })
   }
 
   onSubmit(){
     console.log(this.signupForm)
+    // Reset the form 
+    this.signupForm.reset();
+  }
+
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  // customized validator for user name input
+  forbiddenNameCheck(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenNames.indexOf(control.value) !== -1){
+      return {'nameIsForbiden': true}
+    } else {
+      return null
+    }
+
+  }
+
+  // async validator to use timeout to mimic async
+  forbiddenEmailCheck(control: FormControl): Promise<{[s: string]: boolean}> | Observable<any>{
+    const promise = new Promise<{[s: string]: boolean}>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value == 'test@test.com'){
+          resolve({'emailForBiddenTrue': true})
+        } else {
+          resolve(null);
+        }
+      }, 1500)
+    })
+    return promise;
   }
 
 
