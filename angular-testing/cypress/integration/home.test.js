0699b5c00cceb8f2a0a180b82132f3888c9cfbf1
Ian Seng <ianseng@outlook.com>
2020-05-09 21:22:39 -0400

    complete section 6 e2e testing with cypress

--- angular-testing/cypress/integration/home.test.js
+++ angular-testing/cypress/integration/home.test.js
@@ -1,64 +1,29 @@
+describe("Home Page", () => {
+  beforeEach(() => {
+    cy.fixture("courses.json").as("coursesJSON"); // we are now doing real http request but mocking the instance (the feedback)
 
+    cy.server(); //stimulate mock server (HTTP request)
 
+    cy.route("/api/courses", "@coursesJSON").as("courses"); //@coursesJSON and the @ is to access the variable we have created above. "as is the name"
 
-describe('Home Page', () => {
+    cy.visit("/");
+  });
 
-    beforeEach(() => {
+  it("should display a list of courses", () => {
+    cy.contains("All Courses");
 
-        cy.fixture('courses.json').as("coursesJSON");
+    cy.wait("@courses"); // wait for the courses response to be simulated
 
-        cy.server();
+    cy.get("mat-card").should("have.length", 9);
+  });
 
-        cy.route('/api/courses', "@coursesJSON").as("courses");
+  it("should display the advanced courses", () => {
+    cy.get('.mat-tab-label').should("have.length", 2);
 
-        cy.visit('/');
+    cy.get('.mat-tab-label').last().click(); // .last is to select the second button
 
-    });
+    cy.get('.mat-tab-body-active .mat-card-title').its('length').should('be.greaterThan', 1);
 
-    it('should display a list of courses', () => {
-
-        cy.contains("All Courses");
-
-        cy.wait('@courses');
-
-        cy.get("mat-card").should("have.length", 9);
-
-    });
-
-    it('should display the advanced courses', () => {
-
-        cy.get('.mat-tab-label').should("have.length", 2);
-
-        cy.get('.mat-tab-label').last().click();
-
-        cy.get('.mat-tab-body-active .mat-card-title').its('length').should('be.gt', 1);
-
-        cy.get('.mat-tab-body-active .mat-card-title').first()
-            .should('contain', "Angular Security Course");
-
-    });
-
-
+    cy.get('.mat-tab-body-active .mat-card-title').first().should('contain', "Angular Security Course");
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
